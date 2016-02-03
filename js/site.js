b1ae76f4181117eeb03e6d16a23e8ac0ca8d4bf6
gabriel marcos <gabrielmarcos@gabriels-MacBook-Pro.local>
2016-02-03 10:15:49 -0300

    updated assets

--- js/site.js
+++ js/site.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
+
+	/*
 	$(".btn-twitter").bind("click", function(){
 		$("body").addClass("waiting");
 		setTimeout(function() {
-			//alert("caca");
 			$("body").removeClass("waiting").addClass("divided");
 		}, 3000);
 		return false;
 	});
+*/
 });
