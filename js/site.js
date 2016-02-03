52dbcc14784717ff2fddf13b60cd79664191e220
gabriel marcos <gabrielmarcos@gabriels-MacBook-Pro.local>
2016-02-03 10:51:25 -0300

    added target _blandk to links

--- js/site.js
+++ js/site.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
 
-	/*
-	$(".btn-twitter").bind("click", function(){
-		$("body").addClass("waiting");
-		setTimeout(function() {
-			$("body").removeClass("waiting").addClass("divided");
-		}, 3000);
-		return false;
-	});
-*/
+
 });
